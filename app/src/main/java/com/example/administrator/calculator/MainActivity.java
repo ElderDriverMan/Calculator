484b274e857129589f4e2851dc95d9bedc2b4b88
Administrator <1041827262@qq.com>
2019-01-03 13:38:15 +0800

    添加ActionBar控件，用于隐藏APP的标题

--- app/src/main/java/com/example/administrator/calculator/MainActivity.java
+++ app/src/main/java/com/example/administrator/calculator/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.administrator.calculator;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+
         setContentView(R.layout.activity_main);
     }
 }
