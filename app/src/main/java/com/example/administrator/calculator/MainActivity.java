4a08091cb9c29043ced2c9a3653ad6ec6e126267
tangqigui <46299136+tangqigui@users.noreply.github.com>
2019-01-04 10:26:06 +0800

    控件私有化声明

--- app/src/main/java/com/example/administrator/calculator/MainActivity.java
+++ app/src/main/java/com/example/administrator/calculator/MainActivity.java
@@ -4,8 +4,31 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  Button btn_0;
+    private  Button btn_1;
+    private  Button btn_2;
+    private  Button btn_3;
+    private  Button btn_4;
+    private  Button btn_5;
+    private  Button btn_6;
+    private  Button btn_7;
+    private  Button btn_8;
+    private  Button btn_9;
+    private  Button btn_empty;
+    private  Button btn_delete;
+    private  Button btn_percent;
+    private  Button btn_divide;
+    private  Button btn_multipty;
+    private  Button btn_subtract;
+    private  Button btn_add;
+    private  Button btn_result;
+    private  Button btn_point;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
